a9ec8fd66ce861eeaada4e78b73672e65093563b
manuelchi <manuelchi@gmail.com>
2018-04-22 18:53:48 -0500

    Modificaciones a views

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Rango says hey there! <br/> <a href='/rango/about/'>About</a> ")
+    #Linea inicial:
+    #return HttpResponse("Rango says hey there! <br/> <a href='/rango/about/'>About</a> ")
+    context_dict = {'boldmessage': 'crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+    
 
 def about(request):
-    return HttpResponse("Rango says here is the about page")
+    return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Main page</a>")
