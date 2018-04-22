017acf64260cc3f431b7ace660b619e06c594187
manuelchi <manuelchi@gmail.com>
2018-04-22 18:57:47 -0500

    Correccion

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -27,4 +27,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 	path('', views.index, name='index'),
 	path('rango/', include('rango.urls')),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
